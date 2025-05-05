92a8c206bc6b2c109ebd810b38d882a14e552250
Nikita <example@gmail.com>
2025-05-05 17:22:24 -0400

    services types

--- src/services/StarshipApi.tsx
+++ src/services/StarshipApi.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {StarshipApiResponse} from './types';
 
 export const starshipsApi = createApi({
   reducerPath: 'starshipsApi',
   baseQuery: fetchBaseQuery({baseUrl: 'https://swapi.py4e.com/api/'}),
   endpoints: builder => ({
-    getStarshipsPagination: builder.query<any, number | void>({
+    getStarshipsPagination: builder.query<StarshipApiResponse, number | void>({
       query: (page = 1) => `starships/?page=${page}`,
     }),
-    searchStarships: builder.query<any, string>({
+    searchStarships: builder.query<StarshipApiResponse, string>({
       query: name => `starships/?search=${name}`,
     }),
   }),
